2f3b8185b9b736f30ff44a6c3f95b7fb6c4a5773
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-27 23:03:38 +0200

    finish getGenres logic

--- public/script.js
+++ public/script.js
@@ -4,7 +4,20 @@ const tmdbKey = API_KEY;
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
+const getGenres = async () => {
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl+genreRequestEndpoint+requestParams;
+  try{
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch(error){
+    console.log(error)
+  }
 };
 
 const getMovies = () => {
